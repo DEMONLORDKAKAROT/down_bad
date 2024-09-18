cbd0ac3815fabc6085d681ecca121a9a863a9ef4
DEMONLORDKAKAROT <144598032+DEMONLORDKAKAROT@users.noreply.github.com>
2024-09-18 16:52:30 +0200

    mmmm button

--- back.py
+++ back.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='web/templates')
 
 
+@app.route('/')
 def get_html():
     return render_template('index.html')
 
+
+@app.route('/button_clicked', methods=['POST'])
+def button_cliked():
+    return "<h1>Button was clicked!</h1>"
+
+
 if __name__ == '__main__':
     app.run(debug=True) # runs this in debug mode 
-    
     
     
